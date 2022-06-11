dd03457dbad6fb09cd0e99ba59482c293cfbf010
terry <1342320324@qq.com>
2022-06-11 10:32:23 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author terry
+ * @create 2022-06-11 10:14
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
